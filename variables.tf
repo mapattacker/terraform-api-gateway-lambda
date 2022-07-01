5f17b85911d0d4accb5e5234b026f515fb9aa8f5
Jake Teo <teo.siyang@gmail.com>
2022-07-01 14:04:27 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project" { type = string }
+variable "company" { type = string }
+variable "department" { type = string }
+variable "team" { type = string }
+variable "env" { type = string }
+variable "key_names" { type = list(any) }
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
+variable "region" {
+  type    = string
+  default = "ap-southeast-1"
+}
