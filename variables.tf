e787afa922534cbc08d06f89fb1c7e593c5dad35
Jake Teo <teo.siyang@gmail.com>
2022-07-04 16:34:41 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,8 @@ variable "department" { type = string }
 variable "team" { type = string }
 variable "env" { type = string }
 variable "key_names" { type = list(any) }
+variable "email" { type = string }
+variable "id_expiry" { type = number }
 
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
